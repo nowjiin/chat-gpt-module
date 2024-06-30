4d943e8fd3fd7e3683a89ed65bc87182c9382615
GENIE <hyunjin117@naver.com>
2024-06-30 18:04:05 +0900

    Feat: initial commit

---
+++ src/main/java/com/example/chatgptmodule/ChatGptModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatGptModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatGptModuleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chatgptmodule/ChatGptModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
